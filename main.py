e86361fd6baa434762660ae3abe2c3cf7ca48bd0
fudge123372 <agustinolarry07@gmail.com>
2026-07-06 15:32:22 +0300

    updated

--- main.py
+++ main.py
@@ -1,10 +1,157 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from mydatabase import get_projects, get_suppliers, get_materials , create_project , update_project , delete_project , create_supplier , update_supplier , delete_supplier , login_user
 
 app = Flask(__name__)
+app.secret_key = "buildtrack123"
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+
+    # User must be logged in
+    if "user" not in session:
+        return redirect("/login")
+
+    # Only Administrators can access Projects
+    if session["role"] != "Administrator":
+        return "Access Denied! Only Administrators can view Projects.", 403
+    projects = get_projects()
+    return render_template(
+        "projects.html",
+        active_page="projects",
+        projects=projects
+    )
+
+@app.route("/suppliers")
+def suppliers():
+    suppliers = get_suppliers()
+    return render_template(
+        "suppliers.html",
+        active_page="suppliers",
+        suppliers=suppliers
+    )
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user" not in session:
+
+        return redirect("/login")
+
+    projects = get_projects()
+    suppliers = get_suppliers()
+
+    return render_template(
+        "dashboard.html",
+        active_page="dashboard",
+        total_projects=len(projects),
+        total_suppliers=len(suppliers),
+        projects=projects
+    )
+
+@app.route("/materials")
+def materials():
+    materials = get_materials()
+    return render_template(
+        "materials.html",
+        active_page="materials",
+        materials=materials
+    )
+
+@app.route("/add_project", methods=["GET", "POST"])
+def add_project():
+    if request.method == "POST":
+        create_project(
+            request.form["project_name"],
+            request.form["location"],
+            request.form["budget"]
+        )
+        return redirect(url_for("projects"))
+    return render_template(
+        "add_project.html",
+        active_page="projects"
+    )
+
+@app.route("/edit_project/<int:project_id>", methods=["GET", "POST"])
+def edit_project(project_id):
+    if request.method == "POST":
+        update_project(
+            project_id,
+            request.form["budget"]
+        )
+        return redirect("/projects")
+    project = get_project(project_id)
+    return render_template(
+        "edit_project.html",
+        project=project,
+        active_page="projects"
+    )
+
+@app.route("/delete_project/<int:project_id>")
+def remove_project(project_id):
+    delete_project(project_id)
+    return redirect("/projects")
+
+@app.route("/add_supplier", methods=["GET","POST"])
+def add_supplier():
+    if request.method == "POST":
+        create_supplier(
+            request.form["supplier_name"],
+            request.form["phone"],
+            request.form["email"]
+        )
+        return redirect(url_for("suppliers"))
+    return render_template(
+        "add_supplier.html",
+        active_page="suppliers"
+    )
+
+@app.route("/edit_supplier/<int:supplier_id>", methods=["GET","POST"])
+def edit_supplier(supplier_id):
+    if request.method == "POST":
+        update_supplier(
+            supplier_id,
+            request.form["supplier_name"],
+            request.form["phone"],
+            request.form["email"]
+        )
+        return redirect(url_for("suppliers"))
+    supplier = get_supplier(supplier_id)
+    return render_template(
+        "edit_supplier.html",
+        supplier=supplier,
+        active_page="suppliers"
+    )
+
+@app.route("/delete_supplier/<int:supplier_id>")
+def remove_supplier(supplier_id):
+    delete_supplier(supplier_id)
+    return redirect(url_for("suppliers"))
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = login_user(username, password)
+        if user:
+            session["user"] = user[1]   # username
+            session["role"] = user[4]   # role
+            return redirect("/dashboard")
+        return render_template(
+            "login.html",
+            error="Invalid username or password"
+        )
+    return render_template("login.html")
+    return render_template(
+        "login.html",
+        error=None
+    )
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- mydatabase.py
+++ mydatabase.py
@@ -10,43 +10,30 @@ conn = psycopg2.connect(
 
 # CREATE
 def create_project(project_name, location, budget):
-
     cur = conn.cursor()
-
     sql = """
     INSERT INTO projects
     (project_name, location, budget)
     VALUES (%s, %s, %s)
     """
-
     cur.execute(sql, (project_name, location, budget))
-
     conn.commit()
-
     cur.close()
-
     print("Project added successfully")
 
 
 # READ
 def get_projects():
-
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM projects")
-
     projects = cur.fetchall()
-
     cur.close()
-
     return projects
 
 
 # UPDATE
 def update_project(project_id, budget):
-
     cur = conn.cursor()
-
     cur.execute("""
         UPDATE projects
         SET budget = %s
@@ -54,35 +41,140 @@ def update_project(project_id, budget):
     """, (budget, project_id))
 
     conn.commit()
-
     cur.close()
-
     print("Project updated")
 
 
 # DELETE
 def delete_project(project_id):
-
     cur = conn.cursor()
-
     cur.execute("""
         DELETE FROM projects
         WHERE project_id = %s
     """, (project_id,))
-
     conn.commit()
-
     cur.close()
-
     print("Project deleted")
 
 
-if __name__ == "__main__":
 
+
+# CREATE
+def create_supplier(supplier_name, phone, email):
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO suppliers
+        (supplier_name, phone, email)
+        VALUES (%s, %s, %s)
+    """, (supplier_name, phone, email))
+    conn.commit()
+    cur.close()
+
+
+# READ
+def get_suppliers():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM suppliers")
+    suppliers = cur.fetchall()
+    cur.close()
+    return suppliers
+
+
+def delete_supplier(supplier_id):
+    cur = conn.cursor()
+    cur.execute("""
+        DELETE FROM suppliers
+        WHERE supplier_id=%s
+    """, (supplier_id,))
+    conn.commit()
+    cur.close()
+
+def create_material(material_name, category, unit, quantity, unit_price):
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO materials
+        (material_name, category, unit, quantity, unit_price)
+        VALUES (%s,%s,%s,%s,%s)
+    """,(material_name, category, unit, quantity, unit_price))
+    conn.commit()
+    cur.close()
+
+def get_materials():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM materials")
+    materials = cur.fetchall()
+    cur.close()
+    return materials
+
+def update_material(material_id, quantity):
+    cur = conn.cursor()
+    cur.execute("""
+        UPDATE materials
+        SET quantity=%s
+        WHERE material_id=%s
+    """,(quantity, material_id))
+    conn.commit()
+    cur.close()
+
+def delete_material(material_id):
+    cur = conn.cursor()
+    cur.execute("""
+        DELETE FROM materials
+        WHERE material_id=%s
+    """,(material_id,))
+    conn.commit()
+    cur.close()
+
+def get_project(project_id):
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT * FROM projects
+        WHERE project_id=%s
+    """, (project_id,))
+    project = cur.fetchone()
+    cur.close()
+    return project
+
+
+def get_supplier(supplier_id):
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT * FROM suppliers
+        WHERE supplier_id=%s
+    """, (supplier_id,))
+    supplier = cur.fetchone()
+    cur.close()
+    return supplier
+
+def update_supplier(supplier_id, supplier_name, phone, email):
+    cur = conn.cursor()
+    cur.execute("""
+        UPDATE suppliers
+        SET supplier_name=%s,
+            phone=%s,
+            email=%s
+        WHERE supplier_id=%s
+    """,(supplier_name, phone, email, supplier_id))
+    conn.commit()
+    cur.close()
+
+def login_user(username, password):
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT * FROM users
+        WHERE username=%s
+        AND password=%s
+    """, (username, password))
+    user = cur.fetchone()
+    cur.close()
+    return user
+
+
+
+if __name__ == "__main__":
     create_project(
         "Apartment Block",
         "Nairobi",
         5000000
     )
-
     print(get_projects())
